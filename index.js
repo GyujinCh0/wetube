c2c043f46ce662cb5acef968e0b25f0c8cbb5628
Gyujin Cho <och1939420@gmail.com>
2020-08-07 21:33:52 +0900

    Middlewares adding

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("good");
+import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express();
+
+const PORT = 4000;
+
+const hadleListening=()=>console.log(`Listening on: http://localhost:${PORT}`);
+ 
+const handleHome=(req, res)=>res.send("All Hail to the ffhome");
+ 
+const handProfile=(req,res)=>res.send("You on Profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(logger("dev"));
+
+app.get("/",handleHome);
+app.get("/profile",handProfile)
+app.listen(PORT,hadleListening);
