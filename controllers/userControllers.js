7b007cb7676ec0d2bee2223e045935520ecf9e38
Gyujin Cho <och1939420@gmail.com>
2020-08-09 23:19:03 +0900

    Adding Join Controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,21 @@
-export const join = (req, res) => res.render("join",{pageTitle: "Join"});
+import routes from "../routes"
+export const getJoin = (req, res) =>{ 
+    res.render("join",{pageTitle: "Join"});
+};
+export const postJoin=(req,res)=>{
+    const {
+        body: {name,email,password,passwordVerify}
+    }=req;
+    if(password!==passwordVerify){
+        res.status(400);
+        res.render("join", {pageTitle:"Join"});
+    }else{
+        //To Do: Rejister User
+        //To Do: User Login
+        res.redirect(routes.home);
+    }
+    res.render("join", {pageTitle:"Join"});
+};
 export const login = (req, res) => res.render("login",{pageTitle: "Login"});
 export const logout = (req, res) => res.render("logout");
 export const users =(req,res)=>res.render("users");
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 23452,
+    title: "dfsf",
+    description: "dfsdfsf",
+    views: "334",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1313,
+      name: "dddd",
+      email: "sdfsdf@dff.com",
+    }
+  },
+  {
+    id: 2352,
+    title: "dfsfdddd",
+    description: "dfsdfsf",
+    views: "334",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1313,
+      name: "dddd",
+      email: "sdfsdf@dff.com",
+    }
+  },
+  {
+    id: 252,
+    title: "dfs7777f",
+    description: "dfsdfsf",
+    views: "334",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1313,
+      name: "dddd",
+      email: "sdfsdf@dff.com",
+    }
+  },
+  {
+    id: 23452,
+    title: "dfsf",
+    description: "dfsdfsf",
+    views: "334",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1313,
+      name: "dddd",
+      email: "sdfsdf@dff.com",
+    }
+  },
+]
