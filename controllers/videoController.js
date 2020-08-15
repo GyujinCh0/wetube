967d2d34e54b1a7fba04fb521dcba6da0abee978
Gyujin Cho <och1939420@gmail.com>
2020-08-16 00:24:26 +0900

    Adding uploding

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
-import { videos } from "../db";
 import routes from "../routes";
-export const home = (req, res) => {
+import Video from "../models/Video"
+export const home = async(req, res) => {
+  try{
+    const videos = await Video.find({});
   res.render("home", { pageTitle: "Home", videos });
+  } catch(error){
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -14,12 +20,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: {path}
   } = req;
+  const newVideo = await Video.create({
+    fileURL: path,
+    title: title,
+    description:description
+  });
   // To Do: Upload and save video
-  res.redirect(routes.videoDetail(1313));
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
