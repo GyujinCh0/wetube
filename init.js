01bbcf49ecd96c74d988109c74f934af9f2dc4a1
Gyujin Cho <och1939420@gmail.com>
2021-02-27 11:56:03 +0900

    add User model

--- init.js
+++ init.js
@@ -1,9 +1,13 @@
 import "./db";
-import app from "./app";
 import dotenv from "dotenv";
+import app from "./app";
+
 dotenv.config();
-import "./models/Video";
+
 import "./models/Comment";
+import "./models/User";
+import "./models/Video";
+
 const PORT = process.env.PORT || 4000;
 
 const hadleListening = () => console.log(`Listening on: http://loclahost:${PORT}`);
