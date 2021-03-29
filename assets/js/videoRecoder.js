501149b2082544f2233afe9f344cf4fe6168bf1e
Gyujin Cho <och1939420@gmail.com>
2021-03-29 23:01:06 +0900

    made videoRecord(fin)

--- assets/js/videoRecoder.js
+++ assets/js/videoRecoder.js
@@ -3,16 +3,29 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData= (event)=>{
-
+  const {data: videoFile}=event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
 }
 
 const startRecording= ()=>{
   const videoRecorder=new MediaRecorder(streamObject);
   videoRecorder.start();
-  videoRecorder.addEventListener("dataavailable",handleVideoData
-  );
+  videoRecorder.addEventListener("dataavailable",handleVideoData);
+  recordBtn.addEventListener('click', stopRecording);
+}
+
+const stopRecording = ()=>{
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
 }
 
 const getVideo = async() => {
